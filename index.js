f6c7189bd1e531fc0b8882f53b7505518d566d2d
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-15 13:00:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require("express")
-const home = require("./routes/home")
 const User = require("./routes/UserRoutes")
 
 const app = express()
 app.use(express.json())
 
 // Routes
-app.use("/", User)
+app.use("/api", User)
 
 // connection
 const port = process.env.PORT || 9001
