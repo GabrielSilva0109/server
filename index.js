d399d74192b51dbe74df4784d1cdfbf8f3cb504f
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-15 13:15:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.json())
 // Routes
 app.use("/api", User)
 
+app.get("/", async (req, res, next) => {
+    res.send('Bem vindo ao Servidor')
+})
+
 // connection
 const port = process.env.PORT || 9001
 app.listen(port, () => console.log(`Listening to port ${port}`))
