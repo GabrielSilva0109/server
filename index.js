e4850b933e625053b545141cb81bc8835132b2fb
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-15 12:47:00 -0300

    1

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// Import packages
 const express = require("express")
-const home = require("./routes/home")
 
 // Middlewares
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 // Routes
-app.use("/home", home);
+app.use("/api")
+
+app.get('/', (req, res) => {
+    res.send("bem vindo")
+})
 
 // connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+const port = process.env.PORT || 9001
+
+app.listen(port, () => console.log(`Listening to port ${port}`))
