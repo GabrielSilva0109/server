2d69cecc406d3ffb6f0a954578ffeaf5b2d5c386
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-15 13:17:05 -0300

    Update UserRoutes.js

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/user", async (req, res, next) => {
+router.get("/user", async (req, res) => {
     res.send('Usuarios')
 })
 
