f6b5649ac68010642ba8221afa3a2be3aaec5ea1
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-16 12:07:09 -0300

    21

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express")
 const app = express()
-
 require('dotenv').config()
-const pool = require('./db')
-const User = require("./routes/UserRoutes")
 
 app.use(express.json())
 
