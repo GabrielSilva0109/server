1209bd0a7c26bba7a2d6e35c3a40e1e439c3507c
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 11:31:55 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const mysql = require('mysql12');
+const mysql = require('mysql2');
 
 const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`;
 const connection = mysql.createConnection(urlDB);
