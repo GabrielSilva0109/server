0dc5d1a0ce6c2c5f1c752c5e4f0afa3609cb63ac
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 11:28:51 -0300

    1

--- config.js
+++ config.js
@@ -1,15 +1,7 @@
-require('dotenv').config()
-const mysql = require('mysql12')
+require('dotenv').config();
+const mysql = require('mysql12');
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`
-const connection = mysql.createConnection(urlDB)
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`;
+const connection = mysql.createConnection(urlDB);
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Erro ao conectar ao banco de dados:', err.stack)
-        return
-    }
-    console.log('Conectado ao banco de dados como id ' + connection.threadId)
-})
-
-module.exports = connection
+module.exports = connection;
--- index.js
+++ index.js
@@ -2,15 +2,25 @@ const express = require("express")
 const app = express()
 require('dotenv').config()
 
+const connection = require('./config')
+
 app.use(express.json())
-
-
 
 app.get("/", async (req, res, next) => {
     res.send('Bem vindo ao Servidor')
 })
 
-
 // connection
-const port = process.env.PORT || 9001
-app.listen(port, () => console.log(`Listening to port ${port}`))
+const port = process.env.PORT || 9001;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`);
+
+    // Tenta conectar ao banco de dados
+    connection.connect((err) => {
+        if (err) {
+            console.error('Erro ao conectar ao banco de dados:', err.stack);
+            return;
+        }
+        console.log('Conectado ao banco de dados como id ' + connection.threadId);
+    });
+});
