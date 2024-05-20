248ad488a281935fc228698b584e7ccc351d0310
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 11:19:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 require('dotenv').config();
 const mysql = require("mysql2/promise");
 
 // Conexão com o banco de dados
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`
-let connection
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}/${process.env.MYSQLDATABASE}`;
+let connection;
 
 mysql.createConnection(urlDB)
   .then(conn => {
@@ -14,27 +14,30 @@ mysql.createConnection(urlDB)
   })
   .catch(err => {
     console.error("Error connecting to the database:", err);
-  })
-  
-app.use(express.json())
+  });
 
-
+app.use(express.json());
 
 app.get("/", async (req, res, next) => {
-    res.send('Bem vindo ao Servidor')
-})
+  res.send('Bem vindo ao Servidor');
+});
 
 // Rota para buscar todos os dados da tabela users
 app.get("/users", async (req, res, next) => {
-    try {
-      const [rows] = await connection.execute("SELECT * FROM users");
-      res.json(rows);
-    } catch (err) {
-      next(err);
-    }
-  });
+  try {
+    const [rows] = await connection.execute("SELECT * FROM users");
+    res.json(rows);
+  } catch (err) {
+    next(err);
+  }
+});
 
+// Middleware de tratamento de erros
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-// connection
-const port = process.env.PORT || 9001
-app.listen(port, () => console.log(`Listening to port ${port}`))
+// Iniciando o servidor
+const port = process.env.PORT || 9001;
+app.listen(port, () => console.log(`Listening to port ${port}`));
