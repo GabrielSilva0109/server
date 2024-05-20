7d14b2f572c8af49299661b91874ec6992d88b08
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 10:34:57 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+require("dotenv").config()
+const mysql = require("mysql12")
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLDATABASE}`
+const connection = mysql.createConnection(urlDB)
+
+module.exports = connection
