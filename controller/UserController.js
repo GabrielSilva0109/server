41cdfb54a546059f99e1e682494472b73478a274
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 12:22:48 -0300

    1

--- controller/UserController.js
+++ controller/UserController.js
@@ -6,9 +6,38 @@ const getUsers = (req, res) => {
             return res.status(500).json({ error: error.message });
         }
         res.status(200).json(results);
-    });
-};
+    })
+}
+
+const getUserById = (req, res) => {
+    connection.query('SELECT * FROM users', (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        res.status(200).json(results);
+    })
+}
+
+const createUser = (req, res) => {
+    const { name, password, email, birth, cpf, cep } = req.body
+
+    if (!name || !password || !email) {
+        return res.status(400).json({ error: 'Required fields' })
+    }
+
+    const hashedPassword = bcrypt.hashSync(password, 10)
+
+    connection.query("INSERT INTO users (`name`, `password`,`email`,`birth`, `cpf`, `cep`) VALUES (?,?,?,?,?,?);", [name, hashedPassword, email, birth, cpf, cep], (error, results) => {
+        if (error) {
+            return res.status(500).json({ error: error.message });
+        }
+        res.status(201).json({ id: results.insertId, name, email });
+    })
+}
+
 
 module.exports = {
-    getUsers
+    getUsers,
+    getUserById,
+    createUser,
 };
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const userController = require('..//controller/UserController')
+const userController = require('../controller/UserController')
 
 router.get('/users', userController.getUsers)
+router.get('/user/:id', userController.getUserById)
+router.post('/user', userController.createUser)
 
 module.exports = router
