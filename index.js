0833ab063b86fb278de20e9e131575d64b1136f4
Gabriel <100786939+GabrielSilva0109@users.noreply.github.com>
2024-05-20 11:10:41 -0300

    1

--- index.js
+++ index.js
@@ -4,9 +4,22 @@ require('dotenv').config()
 
 app.use(express.json())
 
+
+
 app.get("/", async (req, res, next) => {
     res.send('Bem vindo ao Servidor')
 })
+
+// Rota para buscar todos os dados da tabela users
+app.get("/users", async (req, res, next) => {
+    try {
+      const [rows] = await connection.execute("SELECT * FROM users");
+      res.json(rows);
+    } catch (err) {
+      next(err);
+    }
+  });
+  
 
 // connection
 const port = process.env.PORT || 9001
